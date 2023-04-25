31cf254f94118b2556646bb23dc2b7bb32a8d3f1
Adam Oberle <oberle.adam@gmail.com>
2023-04-25 09:36:09 -0500

    started work on addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection( title, artist, yearPublished ){
+    let album = {
+        title: '',
+        artist: '',
+        yearPublished: ''
+    }
+    collection.push( album );
+    console.log( `We have added ${ album } to our collection!`);
+}
+
+addToCollection( 'Hello Rockview', 'Less Than Jake', 1998 );
