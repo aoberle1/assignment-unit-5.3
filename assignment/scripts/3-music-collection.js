5e55a0dd7db77d1b5466fa1d83d0f39c938d153b
Adam Oberle <oberle.adam@gmail.com>
2023-04-25 10:00:52 -0500

    addToCollection function tested and working

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,13 +2,16 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection( title, artist, yearPublished ){
+    // creating properties for albums using this.,
+    //this. is referring to the object we are creating in the function addToCollection
     let album = {
-        title: '',
-        artist: '',
-        yearPublished: ''
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
     }
     collection.push( album );
-    console.log( `We have added ${ album } to our collection!`);
+    console.log( `We have added ${ title } by ${ artist } to our collection!`);
+    
 }
 
 addToCollection( 'Hello Rockview', 'Less Than Jake', 1998 );
